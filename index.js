a362a64fbd85e624fcb20369885099c0bd04c068
Matt Dziuban <mrdziuban@gmail.com>
2017-02-09 17:59:04 -0500

    Remove es6 syntax, add icons.

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const Elm = require('./App.elm');
-const app = Elm.App.embed(document.getElementById('elm'));
+var Elm = require('./App.elm');
+var app = Elm.App.embed(document.getElementById('elm'));
 app.ports.selectOutput.subscribe(function() { document.getElementById('sql-output').select(); });
