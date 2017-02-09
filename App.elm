c53e9f86d213b9c3b42829e4b249d1821f3c342f
Matt Dziuban <mrdziuban@gmail.com>
2017-02-09 17:59:04 -0500

    Migrate to Elm, setup build with webpack and auto-dist on git commit.

---
+++ App.elm
@@ -0,0 +1,76 @@
+port module App exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onFocus, onInput)
+import Result
+import String
+import SQLFormatter
+
+defaultSpaces: Int
+defaultSpaces = 2
+
+main: Program Never Model Msg
+main =
+  Html.program
+    {
+      init = init
+    , view = view
+    , update = update
+    , subscriptions = \_ -> Sub.none
+    }
+
+type alias Model =
+  {
+    input: String
+  , output: String
+  , spaces: Int
+  }
+
+init: (Model, Cmd Msg)
+init =
+  {
+    input = ""
+  , output = ""
+  , spaces = defaultSpaces
+  } ! []
+
+port selectOutput: () -> Cmd msg
+
+type Msg = Input String | Spaces String | Select
+
+update: Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Input text ->
+      { model
+          | input = text
+          , output = SQLFormatter.format text model.spaces
+      } ! []
+    Spaces text ->
+      let
+        spacesInt = Result.withDefault defaultSpaces (String.toInt text)
+      in
+        { model
+            | spaces = spacesInt
+            , output = SQLFormatter.format model.input spacesInt } ! []
+    Select ->
+      (model, selectOutput ())
+
+view: Model -> Html Msg
+view model =
+  div [ class "container" ]
+    [ h1 [ class "my-4" ] [ text "SQL Formatter" ]
+    , div [ class "form-inline mb-3" ]
+      [ label [ for "sql-spaces", class "h4 mr-3" ] [ text "Spaces" ]
+      , input [ id "sql-spaces", class "form-control", type_ "number", value (toString model.spaces), onInput Spaces ] []
+      ]
+    , div [ class "form-group" ]
+      [ label [ for "sql-input", class "d-flex h4 mb-3" ] [ text "Input" ]
+      , textarea [ id "sql-input", class "form-control code", placeholder "Enter SQL...", rows 15, onInput Input ] [ text model.input ]
+      ]
+    , div [ class "form-group" ]
+      [ label [ for "sql-output", class "d-flex h4 mb-3" ] [ text "Output" ]
+      , textarea [ id "sql-output", class "form-control code mb-3", rows 28, readonly True, onFocus Select, onClick Select ] [ text model.output ]
+      ]
+    ]
--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-import SQLFormatter from './sql-formatter';
-
-(function() {
-  document.addEventListener('DOMContentLoaded', () => {
-    const input = document.getElementById('sql-input');
-    const output = document.getElementById('sql-output');
-    input.oninput = () => output.value = SQLFormatter.format(input.value);
-  });
-})();
+const Elm = require('./App.elm');
+const app = Elm.App.embed(document.getElementById('elm'));
+app.ports.selectOutput.subscribe(function() { document.getElementById('sql-output').select(); });
